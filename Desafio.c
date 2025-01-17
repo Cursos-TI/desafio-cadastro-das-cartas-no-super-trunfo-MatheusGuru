3d50659fe79c3b08e4296ebf481a0bc7ae63968c
MatheusLsM <122499353+MatheusGuru@users.noreply.github.com>
2025-01-17 12:57:35 +0000

    Simplicando int main

---
+++ Desafio.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    char código[4];
+    char nome[20]; 
+    int populacao;
+    float area;
+    float PIB;
+    int pontos_turísticos;
+
+    printf("Digite o codido da cidade\n");
+    scanf("%s", &código);
+
+    printf("Digite o nome da cidade\n");
+    scanf("%s", &nome);
+
+    printf("Digite a população\n");
+    scanf("%d", &populacao);
+
+    printf("Digite a área\n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB\n");
+    scanf("%f", &PIB);
+    
+    printf("Digite o número de pontos turísticos\n");
+    scanf("%F", &pontos_turísticos);
+
+} 
