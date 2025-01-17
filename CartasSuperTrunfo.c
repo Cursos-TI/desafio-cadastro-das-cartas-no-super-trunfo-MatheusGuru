958b5ced62d89c98efaa40d686531cfbfdde60be
MatheusLsM <122499353+MatheusGuru@users.noreply.github.com>
2025-01-17 14:06:31 +0000

    Desafio completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
+    char codigo[4];
+    char nome[20]; 
+    float populacao;
+    float area;
+    float PIB;
+    int pontos_turisticos;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    printf("Digite o codido da cidade\n");
+    scanf("%s", codigo);
+
+    printf("Digite o nome da cidade\n");
+    scanf("%s", nome);
+
+    printf("Digite a população\n");
+    scanf(" %f", &populacao);
+
+    printf("Digite a área\n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB\n");
+    scanf(" %f", &PIB);
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Digite o número de pontos turísticos\n");
+    scanf("%d", &pontos_turisticos);
+
+    printf("codigo da cidade: %s\n", codigo);
+    printf("Nome da cidade: %s\n", nome);
+    printf("População: %.2f mil\n", populacao);
+    printf("Área: %.2f km\n", area);
+    printf("PIB: %.2f mil\n", PIB);
+    printf("Número de pontos turisticos: %d\n", pontos_turisticos);
 
     return 0;
-}
+
+} 
